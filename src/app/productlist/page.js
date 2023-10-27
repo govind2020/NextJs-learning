45ec1668b630f0ed0d370754abd57f944e5942e7
govind2020 <govind@setoo.co>
2023-10-27 07:36:33 +0530

    remove unnessary consoles

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -10,7 +10,6 @@ async function FetchData (){
 
 async function ProductListing () {
   let allProductData = await FetchData()
-  console.log("allProductData", allProductData)
   return (
     <div>
       <h1>This is The Server Side Rendering!</h1>
@@ -18,7 +17,6 @@ async function ProductListing () {
         allProductData.map((productItem)=>{
           return (
           <>
-           {console.log("allProductData", productItem)}
             <h4>This is Product Name:- {productItem.title}</h4>
             <h4>This is Product Brand:- {productItem.brand}</h4>
             <h4>This is Product Description:- {productItem.description}</h4>
