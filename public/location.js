06103e8fbb22373eab18c531be2cd95055ee4254
govind2020 <govind@setoo.co>
2023-10-28 18:23:37 +0530

    static - component with using geo location

---
+++ public/location.js
@@ -0,0 +1,15 @@
+console.log("file is loaded starting!")
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+function showPosition(position) {
+    console.log(position)
+
+  }
+getLocation();
