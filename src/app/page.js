f5a4a7420cb7fb779d7450a5b26604742ff28e0e
govind2020 <govind@setoo.co>
2023-10-28 15:01:14 +0530

    feat(conditionStyling): apply condition stylings

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import Link from 'next/link'
+'use client'
+import React, { useState } from 'react'
+import conditionalStyling from './custom.module.css'
 
 function Home() {
+  const {orange} = conditionalStyling
+  const [headingColor, setHeadingColor] = useState('red')
   return (
     <>
-    <h1>You Can Fetch the Data in Product page in Client Component</h1>
-    <Link href="/productlist" >Go to ProductList Page</Link>
+    <h1 className={headingColor=='red'?conditionalStyling.red:conditionalStyling.green} >Conditional Styling!</h1>
+    <p className={orange} >Dummy Text for style</p>
+    <button onClick={()=>setHeadingColor('green')} >Update Color</button>
     </>
   )
 }
